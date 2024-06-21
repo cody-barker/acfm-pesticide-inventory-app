25784fb7d9420c6dc3b676a9049cf22369c8d4a4
Cody <codybarker.or@gmail.com>
2024-06-21 11:37:32 -0700

    migrate Product model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_06_21_001409) do
+ActiveRecord::Schema.define(version: 2024_06_21_183303) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "epa_reg"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
