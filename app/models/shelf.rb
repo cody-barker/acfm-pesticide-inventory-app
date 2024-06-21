7b4cc2fa7d184528de155965b92df158a39a010d
Cody <codybarker.or@gmail.com>
2024-06-21 16:05:14 -0700

    add model validations and relationships

--- app/models/shelf.rb
+++ app/models/shelf.rb
@@ -1,3 +1,7 @@
 class Shelf < ApplicationRecord
-  
+  # has_many :containers
+  # has_many :users, through: :containers
+  # has_many :contents, through: :containers
+  validates :number, presence: true
+  validates :number, uniqueness: true
 end
