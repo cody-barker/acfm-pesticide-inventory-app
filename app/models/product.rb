13a0276353f0a28557da329775e016bdf492728f
Cody <codybarker.or@gmail.com>
2024-06-21 11:37:09 -0700

    add relationships and validations to Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+  has_many :contents
+  has_many :containers, through: :contents
+  validates :name, presence: true, length: {minimum: 2}
+  validates :name, uniqueness: true
 end
