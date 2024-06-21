69a8abc01d4c467a1925ea287a467b364ad85c56
Cody <codybarker.or@gmail.com>
2024-06-21 11:54:40 -0700

    add product context to Products page

--- client/src/pages/Products.js
+++ client/src/pages/Products.js
@@ -1,4 +1,9 @@
+import { useContext } from "react";
+import { ProductContext } from "../contexts/ProductContext";
+
 function Products() {
+  let { products } = useContext(ProductContext);
+
   return <p>Products</p>;
 }
 
