a35a3eadc4c6356c485fbe7ec79058ce0bf55a37
Cody <codybarker.or@gmail.com>
2024-06-20 17:05:39 -0700

    add user controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,30 @@
 class UsersController < ApplicationController
+
+    def index
+        render json: User.all
+    end
+
+    def show
+        user = User.find_by(id: session[:user_id])
+        render json: user, status: :created
+    end
+
+    def create
+        user = User.create!(user_params)
+        session[:user_id] = user.id
+        render json: user, status: :created
+    end
+
+    private
+
+    def find_user
+        User.find(params[:id])
+    end
+
+    def user_params
+        params.permit(
+            :username, 
+            :password
+        )
+    end
 end
