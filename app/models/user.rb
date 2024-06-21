2efe2be777f0c90a496484039171cae0dd1f6b75
Cody <codybarker.or@gmail.com>
2024-06-20 17:01:13 -0700

    add active record associations and validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
-  
+  has_secure_password
+  has_many :containers
+  has_many :contents, through: :containers
+  has_many :shelves, through: :containers
+  has_many :products, through: :contents
+  validates :username, presence: true, length: {minimum: 2}
+  validates :username, uniqueness: true
+  validates :username, format: {without: /\s/, message: "cannot contain spaces"}
+  validates :password, length: {minimum: 4, maximum: 16}, format: {without: /\s/, message: "cannot contain spaces"}
 end
