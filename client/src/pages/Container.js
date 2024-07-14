e6e80eac6285b3c5ad94de4a95b4c7e4fd68462d
Cody <codybarker.or@gmail.com>
2024-07-14 13:29:21 -0700

    create initial container table

--- client/src/pages/Container.js
+++ client/src/pages/Container.js
@@ -1,15 +1,47 @@
 import { useContext } from "react";
 import { UserContext } from "../contexts/UserContext";
 import { useParams } from "react-router-dom";
+import { ProductsContext } from "../contexts/ProductsContext";
 
 function Container() {
   let { id } = useParams();
   id = parseInt(id);
   const { user, setUser } = useContext(UserContext);
-
   const container = user.containers.find((container) => container.id === id);
-  console.log(id);
-  return <div></div>;
+  const { products } = useContext(ProductsContext);
+  console.log(container);
+
+  return (
+    <div>
+      <table className="inventory-table">
+        <thead>
+          <tr>
+            <th>Shelf</th>
+            <th>Row</th>
+            <th colSpan={products.length + 1}>Contents</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{container.shelf}</td>
+            <td>{container.row}</td>
+            {container.contents.map((content, index) => {
+              return (
+                <td key={index}>
+                  {content.concentration}% {""}
+                  {
+                    products.find(
+                      (product) => product.id === content.product_id
+                    )?.name
+                  }
+                </td>
+              );
+            })}
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default Container;
