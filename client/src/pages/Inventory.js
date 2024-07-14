b04cfd3393be2ef3e37692ff84942063c17f1437
Cody <codybarker.or@gmail.com>
2024-07-14 10:29:20 -0700

    iterate over containers and view them in a table on inventory page

--- client/src/pages/Inventory.js
+++ client/src/pages/Inventory.js
@@ -1,6 +1,35 @@
-function Inventory() {
+import { useContext, useState } from "react";
+import { UserContext } from "../contexts/UserContext";
 
-  return <p>Inventory</p>;
+function Inventory() {
+  //get the containers state through context
+  //render a table of all containers with their shelves and rows
+  //eventually render their contents as well
+
+  const { user, setUser } = useContext(UserContext);
+  const containers = user.containers.map((container) => {
+    return container;
+  });
+  const tableRows = containers.map((container) => {
+    return (
+      <tr key={container.id}>
+        <td>{container.shelf}</td>
+        <td>{container.row}</td>
+      </tr>
+    );
+  });
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Shelf</th>
+          <th>Row</th>
+        </tr>
+      </thead>
+      <tbody>{tableRows}</tbody>
+    </table>
+  );
 }
 
 export default Inventory;
