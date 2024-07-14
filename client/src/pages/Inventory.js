6f9a6ffd5771337e369a95825d78103eb6c9cde0
Cody <codybarker.or@gmail.com>
2024-07-14 10:58:55 -0700

    iterate over container contents and display contents in inventory table

--- client/src/pages/Inventory.js
+++ client/src/pages/Inventory.js
@@ -1,21 +1,35 @@
 import { useContext, useState } from "react";
 import { UserContext } from "../contexts/UserContext";
-
+import { ProductsContext } from "../contexts/ProductsContext";
 function Inventory() {
   //get the containers state through context
   //render a table of all containers with their shelves and rows
   //eventually render their contents as well
 
   const { user, setUser } = useContext(UserContext);
+  const { products, setProducts } = useContext(ProductsContext);
   const containers = user.containers.map((container) => {
     return container;
   });
+  console.log(containers);
 
   const tableRows = containers.map((container) => {
     return (
       <tr key={container.id}>
         <td>{container.shelf}</td>
         <td>{container.row}</td>
+        {container.contents.map((content, index) => {
+          // Find the product object that matches the product_id
+          let product = products.find(
+            (product) => product.id === content.product_id
+          );
+          // Render a <td> if product is found
+          return product ? (
+            <td key={index}>
+              {content.concentration}% {product.name}
+            </td>
+          ) : null;
+        })}
       </tr>
     );
   });
