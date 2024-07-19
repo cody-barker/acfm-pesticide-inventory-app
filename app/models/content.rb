a6d9b9eb46a85ba536039bf2dfe10d92ab177f42
Cody <codybarker.or@gmail.com>
2024-07-19 12:57:54 -0700

    add custom number validation for concentration

--- app/models/content.rb
+++ app/models/content.rb
@@ -2,4 +2,6 @@ class Content < ApplicationRecord
   belongs_to :container
   belongs_to :product
   validates :concentration, presence: true
+  validates :concentration, presence: true, numericality: { greater_than: 0, less_than_or_equal_to: 100, message: "must be > 0 and <= 100" }
+
 end
