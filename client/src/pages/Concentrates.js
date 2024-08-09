01797e7147e047451fa43c63df92423d43956a31
Cody <codybarker.or@gmail.com>
2024-08-09 13:07:12 -0700

    populate Concentrates table

--- client/src/pages/Concentrates.js
+++ client/src/pages/Concentrates.js
@@ -3,18 +3,42 @@ import { UserContext } from "../contexts/UserContext";
 import { ProductsContext } from "../contexts/ProductsContext";
 
 function Concentrates() {
-  const { user, setUser } = useContext(UserContext);
-  const { products, setProducts } = useContext(ProductsContext);
-  const concentrateComps = user.containers.map((container) => {
-    return (
-      <tr>
-        <td></td>
-      </tr>
-    );
+  const { user } = useContext(UserContext);
+  const { products } = useContext(ProductsContext);
+
+  // Create a function to get the product name by its ID
+  const getProductNameById = (id) => {
+    const product = products.find((product) => product.id === id);
+    return product ? product.name : "Unknown Product";
+  };
+
+  // Create an object to hold the count of containers per product with 100% concentration
+  const productCounts = {};
+
+  user.containers.forEach((container) => {
+    container.contents.forEach((content) => {
+      if (content.concentration === 100) {
+        if (productCounts[content.product_id]) {
+          productCounts[content.product_id] += 1;
+        } else {
+          productCounts[content.product_id] = 1;
+        }
+      }
+    });
   });
 
+  // Map over the productCounts to create table rows
+  const concentrateComps = Object.entries(productCounts).map(
+    ([productId, count]) => (
+      <tr key={productId}>
+        <td>{getProductNameById(Number(productId))}</td>
+        <td>{count}</td>
+      </tr>
+    )
+  );
+
   return (
-    <div>
+    <div className="table-container">
       <table className="table">
         <thead>
           <tr className="table-header-row">
