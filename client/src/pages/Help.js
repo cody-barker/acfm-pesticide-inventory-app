1d86edcf38bb57890c058a464ec8a484af895602
Cody <codybarker.or@gmail.com>
2024-08-16 11:33:03 -0700

    add help section and image

--- client/src/pages/Help.js
+++ client/src/pages/Help.js
@@ -1,5 +1,81 @@
+import React, { useState } from "react";
+
 function Help() {
-  return <div>Help</div>;
+  const [expandedRows, setExpandedRows] = useState([]);
+
+  const topics = [
+    {
+      title: "How do I find premix or concentrate in the chem shed?",
+      content:
+        "Inventory -> All Products -> Select a Product -> All Concentrations (optional) -> Select a Concentration (optional). You may select a second product and concentration if the prescription contains more than one pesticide. Cross reference the Shelving Layout image at the bottom of the page with the container's shelf and row to find its location.",
+    },
+    {
+      title: "How do I add premix or concentrate to the chem shed?",
+      content: "Inventory -> Add a Container.",
+    },
+    {
+      title:
+        "How do I remove premix from the chem shed for a spray trailer/small spray setup?",
+      content:
+        "Inventory -> Click anywhere on a container's row in the table -> Remove. When removing a container, push the rest of the containers in that row to the back, so that others can see which rows have space for more containers.",
+    },
+    {
+      title: "How do I edit a container?",
+      content:
+        "Inventory -> Click anywhere on the container's row in the table -> Edit",
+    },
+    {
+      title:
+        "How do I add new pesticide products that we've never used before?",
+      content:
+        "Products -> Add a Product. Only EPA registered pesticides will have an EPA Reg #. Leave EPA Reg # blank for products like adjuvants, surfactants and dyes.",
+    },
+    {
+      title: "How do I edit a pesticide product name or EPA Reg #?",
+      content:
+        "Products -> Click on a Product Name -> Edit. This will change the pesticide name and/or EPA Reg # for every container in inventory containing that product.",
+    },
+    {
+      title:
+        "We'll no longer be using a specific product in the future. How do I remove it from the application?",
+      content:
+        "Products -> Click on the Product Name -> Remove -> Yes. WARNING: This will remove that product from the products list, and from any containers in inventory containing it, so only do this if you are sure we have used up all of the inventory containing that product, and are sure we'll no longer be using it in the future!",
+    },
+  ];
+
+  const toggleRow = (index) => {
+    if (expandedRows.includes(index)) {
+      setExpandedRows(expandedRows.filter((rowIndex) => rowIndex !== index));
+    } else {
+      setExpandedRows([...expandedRows, index]);
+    }
+  };
+
+  return (
+    <div className="help-wrapper">
+      <div>
+        {topics.map((topic, index) => (
+          <div key={index}>
+            <div className="help-topic" onClick={() => toggleRow(index)}>
+              <strong>{topic.title}</strong>
+            </div>
+            {expandedRows.includes(index) && (
+              <div className="help-content">
+                <p>{topic.content}</p>
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+      <div className="shelving-img-wrapper">
+        <img
+          className="shelving-img"
+          src="assets/shelving.png"
+          alt="Shelving Layout"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Help;
