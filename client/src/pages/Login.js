ebd64af4ecd6481e73eb8cc909d0c8daa285be52
Cody <codybarker.or@gmail.com>
2024-08-24 14:46:55 -0700

    change background image from id to class

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -2,7 +2,7 @@ import LoginForm from "../components/LoginForm";
 
 function Login() {
   return (
-    <div id="background-image" className="wrapper">
+    <div className="wrapper  background-image ">
       <div className="wrapper__form wrapper__form--login">
         <h1 className="wrapper__title">ACFM Pesticide Inventory</h1>
         <LoginForm />
