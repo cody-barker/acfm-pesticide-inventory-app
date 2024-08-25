f427d662835d74a9b6bd432f66e46d2a75054679
Cody <codybarker.or@gmail.com>
2024-08-25 12:42:47 -0700

    adjust padding in footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 function Footer() {
   return (
     <div className="footer">
-      <Link to="https://github.com/cody-barker">
-        <span className="footer__span">Developed by </span>
-        <span className="footer__link">Cody Barker</span>
-        <span className="footer__span"> 2024</span>
+      <span className="footer__span">Developed by </span>
+      <Link className="footer__link" to="https://github.com/cody-barker">
+        <span className="footer__link footer__link--span">Cody Barker</span>
       </Link>
+      <span className="footer__span"> 2024</span>
     </div>
   );
 }
