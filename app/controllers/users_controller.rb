ad49830507487d7bf9a5812ec677dfe63b76f8f7
Cody <codybarker.or@gmail.com>
2024-08-29 11:06:04 -0700

    return user controller to original because show was not working correctly for session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,37 +1,31 @@
 class UsersController < ApplicationController
-  skip_before_action :authorize, only: [:create, :index]
+    # skip_before_action :authorize, only: [:create, :index]
 
-  # GET /users
-  def index
-    render json: User.all
-  end
-
-  # GET /users/:id
-  def show
-    user = find_user
-    if user
-      render json: user
-    else
-      render json: { error: 'User not found' }, status: :not_found
+    def index
+        render json: User.all
     end
-  end
 
-  # POST /users
-  def create
-    user = User.create!(user_params)
-    session[:user_id] = user.id
-    render json: user, status: :created
-  end
+    def show
+        user = User.find_by(id: session[:user_id])
+        render json: user, status: :created
+    end
 
-  private
+    def create
+        user = User.create!(user_params)
+        session[:user_id] = user.id
+        render json: user, status: :created
+    end
 
-  # Find user based on the provided ID
-  def find_user
-    User.find_by(id: params[:id])
-  end
+    private
 
-  # Only allow a list of trusted parameters through
-  def user_params
-    params.require(:user).permit(:username, :password)
-  end
+    def find_user
+        User.find(params[:id])
+    end
+
+    def user_params
+        params.permit(
+            :username, 
+            :password
+        )
+    end
 end
