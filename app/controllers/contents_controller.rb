6e12cf0d1f29ef9ee9d2228a94354e92aecdd88c
Cody <codybarker.or@gmail.com>
2024-08-29 10:31:20 -0700

    updated contents controller to reflect changes to db

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,5 +1,6 @@
 class ContentsController < ApplicationController
   before_action :set_content, only: [:show, :update, :destroy]
+  before_action :set_container, only: [:create, :update, :destroy]
 
   # GET /contents
   def index
@@ -7,38 +8,46 @@ class ContentsController < ApplicationController
     render json: @contents
   end
 
+  # GET /contents/:id
   def show
     render json: @content
   end
 
+  # POST /containers/:container_id/contents
   def create
-    container = Container.find(params[:container_id])
-    content = container.contents.create!(content_params)
+    content = @container.contents.create!(content_params)
     render json: content, status: :created
   end
 
+  # PATCH/PUT /containers/:container_id/contents/:id
   def update
-    container = Container.find(params[:container_id])
-    content = container.contents.find(params[:id])
-    content.update!(content_params)
-    render json: content, status: :accepted
+    if @content.update(content_params)
+      render json: @content, status: :ok
+    else
+      render json: { error: 'Failed to update content' }, status: :unprocessable_entity
+    end
   end
 
+  # DELETE /containers/:container_id/contents/:id
   def destroy
-    container = Container.find(params[:container_id])
-    content = user.contents.find(params[:id])
-    content.destroy
-    render json: content, status: :no_content
+    @content.destroy
+    head :no_content
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_content
-      @content = Content.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def content_params
-      params.permit(:container_id, :product_id, :concentration)
-    end
+  # Set the content based on the provided ID
+  def set_content
+    @content = Content.find(params[:id])
+  end
+
+  # Set the container based on the provided container ID
+  def set_container
+    @container = Container.find(params[:container_id])
+  end
+
+  # Only allow a list of trusted parameters through
+  def content_params
+    params.require(:content).permit(:product_id, :concentration)
+  end
 end
