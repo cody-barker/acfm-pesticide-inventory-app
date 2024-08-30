e8f2eed9d8b03f33351ffa3f824838a0bec5cbfc
Cody <codybarker.or@gmail.com>
2024-08-30 13:23:35 -0700

    comment out skip before action authorize on user index

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,19 @@
 Rails.application.routes.draw do
-# Fallback for React Router, ensuring only non-AJAX and HTML requests are caught
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 
-  # Existing resource routes
   resources :contents, only: [:index, :show, :create, :update, :destroy]
   resources :containers, only: [:index, :show, :create, :update, :destroy]
   resources :products, only: [:index, :show, :create, :update, :destroy]
   resources :users, only: [:index]
   resources :teams
   resources :creation_logs
+  # Routing logic: fallback requests for React Router.
+  # Leave this here to help deploy your app later!
 
-  # Authentication routes
+  # post '/signup', to: 'users#create'
   get '/me', to: 'users#show'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  
 
 end
