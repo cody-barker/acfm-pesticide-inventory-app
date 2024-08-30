f32841d4143d867352935c11fea6422a777fa372
Cody <codybarker.or@gmail.com>
2024-08-30 13:15:51 -0700

    move fallback to bottom of routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,19 @@
 Rails.application.routes.draw do
-  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
-
-
+  # Existing resource routes
   resources :contents, only: [:index, :show, :create, :update, :destroy]
   resources :containers, only: [:index, :show, :create, :update, :destroy]
   resources :products, only: [:index, :show, :create, :update, :destroy]
   resources :users, only: [:index]
   resources :teams
   resources :creation_logs
-  # Routing logic: fallback requests for React Router.
-  # Leave this here to help deploy your app later!
 
-  # post '/signup', to: 'users#create'
+  # Authentication routes
   get '/me', to: 'users#show'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  
 
+  # Fallback for React Router, ensuring only non-AJAX and HTML requests are caught
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+
+  # This needs to be after your other routes
 end
